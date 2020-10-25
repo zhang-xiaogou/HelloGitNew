e937e3e7353535e393030b4479b0e5e681da305a
Administrator <870844964@qq.com>
2020-10-25 12:49:22 +0800

    forTestDevbranch

---
+++ src/main/java/com/mingjunzhang/DevBranchTest.java
@@ -0,0 +1,11 @@
+package com.mingjunzhang;
+
+/**
+ * @author mingjunzhang
+ * @create 2020-10-25 12:48
+ */
+public class DevBranchTest {
+    public static void main(String[] args) {
+        System.out.println("Test devBranch!");
+    }
+}
