23724145ecfbc8223d9b786ea0cb2fd2a03a00c9
Administrator <870844964@qq.com>
2020-10-25 12:30:46 +0800

    forTest

---
+++ src/main/java/com/mingjunzhang/Helloworld.java
@@ -0,0 +1,12 @@
+package com.mingjunzhang;
+
+/**
+ * @author mingjunzhang
+ * @create 2020-10-25 12:29
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
