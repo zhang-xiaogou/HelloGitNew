867d3bd6144750b1c6056e999f785dda4936e52b
Administrator <870844964@qq.com>
2020-10-25 13:07:42 +0800

    forTestDevbranch1

--- src/main/java/com/mingjunzhang/Helloworld.java
+++ src/main/java/com/mingjunzhang/Helloworld.java
@@ -7,6 +7,6 @@ package com.mingjunzhang;
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world！");
     }
 }
